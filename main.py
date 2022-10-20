8c70d2e6a5a6e0b39b02ba8d52f6dac9cff50947
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2022-10-20 11:55:14 -0500

    pieces

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import piece
 
 board = []
 for i in range(9):
     board.append(["","","","","","","",""]) 
 
+pawn1 = piece.Piece(1, 1, "white", "pawn")
+print(pawn1.color)
 print(board)
---
+++ piece.py
@@ -0,0 +1,9 @@
+from tkinter import Y
+
+
+class Piece: 
+    def __init__(self, x, y, color, type): 
+        self.x = x 
+        self.y = y 
+        self.color = color 
+        self.type = type 
