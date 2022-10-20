033e3f1e90024546ba31688d4111df7e0ca4a044
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2022-10-20 13:50:15 -0500

    visuals / draw board

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import piece
 
 board = []
-for i in range(9):
+for i in range(8):
     board.append([None,None,None,None,None,None,None,None]) 
 
 boardSetUp = ["rook", "knight", "bishop", "queen", "king", "bishop", "knight", "rook"]
 for i in range(8): 
     board[0][i] = piece.Piece(i, 0, "black", boardSetUp[i])
     board[1][i] = piece.Piece(i, 0, "black", "pawn")
-    board[6][i] = piece.Piece(i, 0, "white", boardSetUp[i])
-    board[7][i] = piece.Piece(i, 0, "white", "pawn")
-
+    board[7][i] = piece.Piece(i, 0, "white", boardSetUp[i])
+    board[6][i] = piece.Piece(i, 0, "white", "pawn")
 
 print(board)
---
+++ visual.py
@@ -0,0 +1,40 @@
+import main
+
+import sys, pygame
+pygame.init()
+
+pygame.font.init()
+
+size = width, height = 500, 500
+white = 200, 200, 200
+black = 0, 0, 0
+
+screen = pygame.display.set_mode(size)
+
+screen.fill(black)
+
+rect = pygame.Rect(0, 0, width, height)
+pygame.draw.rect(screen, white, rect, 1)
+
+for i in range(8):
+    for j in range(8):  
+        rect = pygame.Rect(j*(width/8), i*(height/8), width/8, height/8)
+        pygame.draw.rect(screen, white, rect, (j+i)%2)
+
+
+font = pygame.font.SysFont('freesanbold.ttf', 30)
+
+for i in range(8):
+    for j in range(8): 
+        if main.board[i][j] != None: 
+            text = font.render(main.board[i][j].type, True, white, black)
+            textRect = text.get_rect()
+            textRect.center = (j*(width/8)+(width/16) , i*(height/8)+(height/16))
+            screen.blit(text, textRect)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+
+
+    pygame.display.flip()
