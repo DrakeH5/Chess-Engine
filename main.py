4e9b43180736a2dd1874ba2aef23d0aff0c4db87
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2022-10-20 13:12:27 -0500

    drawing to board

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ import piece
 
 board = []
 for i in range(9):
-    board.append(["","","","","","","",""]) 
+    board.append([None,None,None,None,None,None,None,None]) 
 
-pawn1 = piece.Piece(1, 1, "white", "pawn")
-print(pawn1.color)
+boardSetUp = ["rook", "knight", "bishop", "queen", "king", "bishop", "knight", "rook"]
+for i in range(8): 
+    board[0][i] = piece.Piece(i, 0, "black", boardSetUp[i])
+    board[1][i] = piece.Piece(i, 0, "black", "pawn")
+    board[6][i] = piece.Piece(i, 0, "white", boardSetUp[i])
+    board[7][i] = piece.Piece(i, 0, "white", "pawn")
+
+
 print(board)
