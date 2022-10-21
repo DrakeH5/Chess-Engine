182b25565bea35e0094aab87059e5d813375ca5f
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2022-10-21 17:07:57 -0500

    comments

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import piece
 
 import pawn
 
+#create board
 board = []
 for i in range(8):
     board.append([None,None,None,None,None,None,None,None]) 
 
+#setup starting board pos
 boardSetUp = ["rook", "knight", "bishop", "queen", "king", "bishop", "knight", "rook"]
 for i in range(8): 
     board[0][i] = piece.Piece(i, 0, "black", boardSetUp[i])
--- pawn.py
+++ pawn.py
@@ -1,16 +1,23 @@
 def PawnMoves(x, y, color, board):
+    #determine which direction pawn can move it based on its color
     if color=="white": 
         direction=-1
     elif color=="black": 
         direction=1
-    possibleMoves=[]
+
+    possibleMoves=[] 
+
     if color=="black" and y==1 or color=="white" and y==6: 
         if board[y+(direction*2)][x] == None:
             possibleMoves.append([y+(direction*2), x])
+
     if  board[y+direction][x+1] != None and board[y+direction][x+1].color != color:  
         possibleMoves.append([y+direction, x+1])
+
     if board[y+direction][x-1] != None and board[y+direction][x-1].color != color:  
         possibleMoves.append([y+direction, x-1])
+        
     if board[y+direction][x] == None: 
         possibleMoves.append([y+direction, x])
-    return possibleMoves
+
+    return possibleMoves #return all possible moves
