596255e79f5ae9ca3339c648b429e2923aa9fbf1
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2022-10-22 10:54:54 -0500

    ♜ + ♝ bug fix

--- rook.py
+++ rook.py
@@ -2,38 +2,50 @@ def RookMoves(x, y, color, board):
     possibleMoves=[] 
     
     
-    for i in range(7): 
+    i = 1 
+    while i < 7: 
         if y+i < 8: 
             if board[y+i][x] == None: 
                 possibleMoves.append([x, y+i])
             elif board[y+i][x].color != color: 
                 possibleMoves.append([x, y+i])
+                i = 8
             else: 
                 i = 8
-    for i in range(7): 
+        i+=1
+    i = 1
+    while i < 7: 
         if y-i > -1: 
             if board[y-i][x] == None: 
                 possibleMoves.append([x, y-i])
             elif board[y-i][x].color != color: 
                 possibleMoves.append([x, y-i])
+                i = 8
             else: 
                 i = 8
-    for i in range(7): 
+        i+=1
+    i = 1 
+    while i < 7: 
         if x+i < 8: 
             if board[y][x+i] == None: 
                 possibleMoves.append([x+i, y])
             elif board[y][x+i].color != color: 
                 possibleMoves.append([x+i, y])
+                i = 8
             else: 
                 i = 8
-    for i in range(7): 
+        i+=1
+    i = 1
+    while i < 7: 
         if x-i > -1: 
             if board[y][x-i] == None: 
                 possibleMoves.append([x-i, y])
             elif board[y][x-i].color != color: 
                 possibleMoves.append([x-i, y])
+                i = 8
             else: 
                 i = 8
+        i+=1
 
 
     return possibleMoves #return all possible moves
